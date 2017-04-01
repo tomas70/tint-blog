32abbb9203dc22761b75ec3a19c3e3a97963c5a6
Tomas <tomas@oneway.lt>
2017-04-01 20:24:24 +0300

    search page

--- search.php
+++ search.php
@@ -9,39 +9,39 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<header class="category-header">
+		<div class="container">
+			<div class="row">
+				<div class="col category-header__text">
+					Search Results:
+				</div>
+			</div>
+		</div>
+	</header>
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'tint-blog' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+		<div class="container">
+			<div class="row">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<div class="col-md-4 cat-posts">
+						<div class="row">
+							<div class="col">
+								<?php the_post_thumbnail(); ?>
+							
+								<h3 class="cat-posts__heading-3"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+								<p class="cat-posts__paragraph"><span class="user-icon-black"> <?php the_author_posts_link(); ?></span> <span class="date-icon-black"> <?php the_time('jS F'); ?></span></p>
+							</div>
+						</div>
+					</div>
+				<?php endwhile; endif; ?>
+				<?php wp_reset_query(); ?>
+			</div>
+		</div>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php
 get_sidebar();
