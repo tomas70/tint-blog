d9d24bbdebc4e34ce7cfb0fad5ab3849b3c85e8b
Tomas <tomas@oneway.lt>
2017-04-10 12:32:55 +0300

    responsive

--- author.php
+++ author.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 <header class="author-header">
 	<div class="container">
-		<div class="row align-items-center">
+		<div class="row align-items-center hidden-sm-down">
 			<?php $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author)); ?>
 			<div class="col-1">
 				<?php echo get_avatar( $curauth, 60 ); ?>
@@ -26,12 +26,27 @@ get_header(); ?>
 				<a class="author-header__url" href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a>
 			</div>
 		</div>
+		<div class="row hidden-sm-up">
+			<div class="col-4">
+				<?php echo get_avatar( $curauth, 100 ); ?>
+			</div>
+			<div class="col-8">
+				<h2 class="author-header__heading-2"><?php echo $curauth->first_name; ?> <?php echo $curauth->last_name; ?> <i class="fa fa-twitter" aria-hidden="true"></i></h2>
+				<p class="author-header__description"><?php echo $curauth->user_description; ?></p>
+				<a class="author-header__url" href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a>
+			</div>
+
+			<div class="col author-header__text">
+				Posts By <?php echo $curauth->first_name; ?>:
+			</div>
+		
+		</div>
 	</div>
 </header>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<div class="container">
+		<div class="container hidden-sm-down">
 			<div class="row">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<div class="col-md-4 cat-posts">
@@ -41,6 +56,23 @@ get_header(); ?>
 							
 								<h3 class="cat-posts__heading-3"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 								<p class="cat-posts__paragraph"><span class="user-icon-black"> <?php the_author_posts_link(); ?></span> <span class="date-icon-black"> <?php the_time('jS F'); ?></span></p>
+							</div>
+						</div>
+					</div>
+				<?php endwhile; endif; ?>
+				<?php wp_reset_query(); ?>
+			</div>
+		</div>
+		<div class="container hidden-sm-up">
+			<div class="row">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<div class="col-12 cat-posts padding-zero">
+						<div class="row align-items-center">
+							<div class="col-4">
+								<?php the_post_thumbnail(array(100, 100)); ?>
+							</div>
+							<div class="col-8">
+								<h3 class="cat-posts__heading-3"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 							</div>
 						</div>
 					</div>
--- category.php
+++ category.php
@@ -21,7 +21,7 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<div class="container">
+		<div class="container hidden-sm-down">
 			<div class="row">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<div class="col-md-4 cat-posts">
@@ -31,6 +31,23 @@ get_header(); ?>
 							
 								<h3 class="cat-posts__heading-3"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 								<p class="cat-posts__paragraph"><span class="user-icon-black"> <?php the_author_posts_link(); ?></span> <span class="date-icon-black"> <?php the_time('jS F'); ?></span></p>
+							</div>
+						</div>
+					</div>
+				<?php endwhile; endif; ?>
+				<?php wp_reset_query(); ?>
+			</div>
+		</div>
+		<div class="container hidden-sm-up">
+			<div class="row">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<div class="col-12 cat-posts padding-zero">
+						<div class="row align-items-center">
+							<div class="col-4">
+								<?php the_post_thumbnail(array(100, 100)); ?>
+							</div>
+							<div class="col-8">
+								<h3 class="cat-posts__heading-3"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 							</div>
 						</div>
 					</div>
